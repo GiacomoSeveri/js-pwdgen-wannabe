c3be1d4212911dd9d02c3ee8cd71ba4c050fad3f
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-02 16:39:51 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// console.log('js ok')
+
+// Prendo l'elemento dal DOM
+const targetElement = document.getElementById('password');
+// console.log(targetElement);
+
+// Chiedere il nome
+const userName = prompt('Come ti chiami?');
+
+// Chiedere il cognome
+const userSurName = prompt('Quale è il tuo cognome?');
+
+// Chiedi il colore preferito
+const userColor = prompt('Quale è il tuo colore prefetito?');
